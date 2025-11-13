03432a24eb7d1a0f2a2442ed5971dbecb2a2dcc6
gearhart <geardev26@gmail.com>
2025-11-12 18:02:52 -0800

    add vault to deploy script

--- script/DeployRBT.s.sol
+++ script/DeployRBT.s.sol
@@ -3,15 +3,14 @@
 pragma solidity ^0.8.24;
 
 import {RebaseToken} from "../src/RebaseToken.sol";
+import {Vault} from "../src/Vault.sol";
 import {CodeConstants, HelperConfig} from "./HelperConfig.s.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployRBT is Script, CodeConstants {
-    function run() external returns (RebaseToken) {
-        return deployContract();
-    }
-
-    function deployContract() public returns (RebaseToken rbt) {
+    function run() external returns (RebaseToken rbt, Vault vault) {
         rbt = new RebaseToken(RBT_NAME, RBT_SYMBOL, INITIAL_INTEREST_RATE);
+        vault = new Vault(rbt);
+        rbt.grantMintAndBurnRole(address(vault));
     }
 }
