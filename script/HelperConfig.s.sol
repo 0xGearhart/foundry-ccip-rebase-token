855653345db215aa1af73b9416caebba82433d12
gearhart <geardev26@gmail.com>
2025-11-17 16:35:47 -0800

    update configuration files

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -9,6 +9,8 @@ abstract contract CodeConstants {
     string public constant RBT_NAME = "Rebase Token";
     string public constant RBT_SYMBOL = "RBT";
     uint256 public constant INITIAL_INTEREST_RATE = 5e10;
+    // RBT Token Pool Info
+    uint8 public constant DECIMAL_PRECISION = 18;
     // mainnet chain id and info
     uint256 public constant ETH_MAINNET_CHAIN_ID = 1;
     // sepolia chain id and info
