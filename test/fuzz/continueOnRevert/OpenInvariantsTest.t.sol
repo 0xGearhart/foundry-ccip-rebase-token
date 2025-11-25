fef14dcf3c4256825363e2d91eb9581eb698d9a1
gearhart <geardev26@gmail.com>
2025-11-24 17:58:48 -0800

    remove VS settings and add folder for continue on revert fuzz tests

---
+++ test/fuzz/continueOnRevert/OpenInvariantsTest.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {DeployRBT} from "../../../script/DeployRBT.s.sol";
+import {RebaseToken} from "../../../src/RebaseToken.sol";
+import {Vault} from "../../../src/Vault.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+contract OpenInvariantsTest is StdInvariant, Test {
+    DeployRBT public deployer;
+    // Handler public handler;
+    RebaseToken rbt;
+    Vault vault;
+
+    function setUp() external {
+        deployer = new DeployRBT();
+        (rbt, vault) = deployer.run();
+        // handler = new Handler(rbt, vault);
+
+        // targetContract(address(handler));
+    }
+    // function invariant_() public pure {}
+}
