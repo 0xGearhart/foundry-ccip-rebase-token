106cdbbd5712afb876bac0e384e6dfe76c8354eb
gearhart <geardev26@gmail.com>
2025-12-16 00:55:39 -0800

    add grantMintAndBurnRole to interface

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -10,6 +10,7 @@ pragma solidity ^0.8.24;
 interface IRebaseToken {
     function mint(address _to, uint256 _amount, uint256 _userInterestRate) external;
     function burn(address _from, uint256 _amount) external;
+    function grantMintAndBurnRole(address _account) external;
     function balanceOf(address _user) external view returns (uint256);
     function principalBalanceOf(address _user) external view returns (uint256);
     function getUserInterestRate(address _user) external view returns (uint256);
