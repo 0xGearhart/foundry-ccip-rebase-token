84d05764b6996b17ee7f9c998722967ce75e7c46
gearhart <geardev26@gmail.com>
2025-12-18 19:24:47 -0800

    add and fix interaction contracts to be called in scripts

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.24;
 
+import {Vault} from "../src/Vault.sol";
 import {CodeConstants} from "./DeployRBT.s.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/contracts/interfaces/IRouterClient.sol";
 import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
@@ -11,20 +12,14 @@ import {IERC20} from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract ConfigurePool is Script, CodeConstants {
-    function run(
-        address _localPool,
-        uint64 _remoteChainSelector,
-        address _remotePool,
-        address _remoteToken,
-        bool _outboundRateLimiterIsEnabled,
-        uint128 _outboundRateLimiterCapacity,
-        uint128 _outboundRateLimiterRate,
-        bool _inboundRateLimiterIsEnabled,
-        uint128 _inboundRateLimiterCapacity,
-        uint128 _inboundRateLimiterRate
-    )
-        public
-    {
+    bool outboundRateLimiterIsEnabled = false;
+    uint128 outboundRateLimiterCapacity = 0;
+    uint128 outboundRateLimiterRate = 0;
+    bool inboundRateLimiterIsEnabled = false;
+    uint128 inboundRateLimiterCapacity = 0;
+    uint128 inboundRateLimiterRate = 0;
+
+    function run(address _localPool, uint64 _remoteChainSelector, address _remotePool, address _remoteToken) public {
         // build array of remote pool addresses in byte format
         bytes[] memory remotePoolAddresses = new bytes[](1);
         // encode remote pool address for bytes array
@@ -38,15 +33,15 @@ contract ConfigurePool is Script, CodeConstants {
             remoteTokenAddress: abi.encode(_remoteToken),
             // configure outbound rate limiter
             outboundRateLimiterConfig: RateLimiter.Config({
-                isEnabled: _outboundRateLimiterIsEnabled,
-                capacity: _outboundRateLimiterCapacity,
-                rate: _outboundRateLimiterRate
+                isEnabled: outboundRateLimiterIsEnabled,
+                capacity: outboundRateLimiterCapacity,
+                rate: outboundRateLimiterRate
             }),
             // configure inbound rate limiter
             inboundRateLimiterConfig: RateLimiter.Config({
-                isEnabled: _inboundRateLimiterIsEnabled,
-                capacity: _inboundRateLimiterCapacity,
-                rate: _inboundRateLimiterRate
+                isEnabled: inboundRateLimiterIsEnabled,
+                capacity: inboundRateLimiterCapacity,
+                rate: inboundRateLimiterRate
             })
         });
         // build array of chains to remove
@@ -59,14 +54,15 @@ contract ConfigurePool is Script, CodeConstants {
     }
 }
 
-contract bridgeTokens is Script {
+contract BridgeTokens is Script {
     function run(
         address _receiver,
         address _tokenToBridge,
         uint256 _amountToBridge,
         uint64 _destinationChainSelector,
         address _routerAddress,
-        address _linkTokenAddress
+        address _linkTokenAddress,
+        uint256 _gasLimit // 0 should be fine on actual chain since it uses default gas but have to send large limit when testing on fork
     )
         public
     {
@@ -75,8 +71,9 @@ contract bridgeTokens is Script {
         tokenAmounts[0] = Client.EVMTokenAmount({token: _tokenToBridge, amount: _amountToBridge});
         // build extra args struct for ccip message
         bytes memory extraArgs =
-            Client._argsToBytes(Client.GenericExtraArgsV2({gasLimit: 0, allowOutOfOrderExecution: false}));
+            Client._argsToBytes(Client.GenericExtraArgsV2({gasLimit: _gasLimit, allowOutOfOrderExecution: false}));
         // build CCIP message
+        vm.startBroadcast(vm.envAddress("DEFAULT_KEY_ADDRESS"));
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
             receiver: abi.encode(_receiver),
             data: "",
@@ -86,14 +83,20 @@ contract bridgeTokens is Script {
         });
         // get dynamic transfer fee in link token. Fee amount dependant upon cross chain message being sent
         uint256 ccipFee = IRouterClient(_routerAddress).getFee(_destinationChainSelector, message);
-
-        vm.startBroadcast();
         // approve link token to pay fee
         IERC20(_linkTokenAddress).approve(_routerAddress, ccipFee);
         // approve tokens to be bridged
         IERC20(_tokenToBridge).approve(_routerAddress, _amountToBridge);
         // bridge tokens
         IRouterClient(_routerAddress).ccipSend(_destinationChainSelector, message);
+        vm.stopBroadcast();
+    }
+}
+
+contract DepositAndMintRbt is Script {
+    function run(address payable vault, uint256 amountToDeposit) public {
+        vm.startBroadcast(vm.envAddress("DEFAULT_KEY_ADDRESS"));
+        Vault(vault).deposit{value: amountToDeposit}();
         vm.stopBroadcast();
     }
 }
